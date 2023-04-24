9e08f7ff2edd31b53a43fa4b44224b76afddaeef
Vaishnavi More <93897423+Vaishnavi-More@users.noreply.github.com>
2023-04-24 23:21:40 +0530

    Tree

---
+++ Tree/Binary tree/avreageOfLevels.cpp
@@ -0,0 +1,30 @@
+//Leetcode Question- 637. Average of Levels in Binary Tree
+
+vector<double> averageOfLevels(TreeNode* root) {
+         vector<double> v;
+        if(root==NULL) return v;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){ 
+            int n =q.size(); 
+            double avg=0, sum=0;
+            for(int i=0; i<n; i++){
+            TreeNode* temp=q.front();
+            q.pop();
+            sum+=temp->val;
+
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
+            }
+            avg=sum/n;
+            v.push_back(avg);
+
+        }
+        return v;
+
+        
+    }
