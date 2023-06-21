ac82d94766de9d9ead11a2ec9eb58966d75c4542
Vaishnavi More <93897423+Vaishnavi-More@users.noreply.github.com>
2023-06-21 20:08:59 +0530

    Cycle in directed graph

--- Graph/cycleInDirectedGraph.cpp
+++ Graph/cycleInDirectedGraph.cpp
@@ -46,5 +46,62 @@ bool helper(int start,vector<int> adj[], vector<bool>&vis, vector<bool> &pathvis
         
     }
 
+//Using BFS technique [Kahn's Algorithm]
+
+/* Kahn's algo is only applicable for DAG.
+It get terminated if DCG is present as a result sorting for all the element can't be obtain.
+This indicate cycle is present in a graph
+*/
+
+ bool isCyclic(int V, vector<int> adj[]) {
+//create indegree array
+	    int indg[V]={0};
+	    
+	    //traverse to calculate indegree
+	    for(int i=0; i<V; i++){
+	        for(auto it: adj[i]){
+	            indg[it]++;
+	        }
+	    }
+	    
+	    //create queue for BFS traversal 
+	    queue<int> q;
+	    
+	    //Push node having indegree 0
+	    for(int i=0; i<V; i++){
+	        if(indg[i]==0){
+	            q.push(i);
+	        }
+	    }
+	    
+	    //create vector to store sorting 
+	    vector<int> topo;
+	    
+	    //traverse until queue becomes empty
+	    while(!q.empty()){
+	        
+	        //store node into vector
+	        int temp=q.front();
+	        q.pop();
+	        topo.emplace_back(temp);
+	        
+	        //traverse it's neighbour node and decrement neighbour node's indegree
+	        for(auto it: adj[temp]){
+	            indg[it]--;
+	            
+	            //if indegree of neighbour node become 0 than it is ready to insert into queue
+	            if(indg[it]==0){
+	                q.push(it);
+	            }
+	        }
+	    }
+
+        //if size of topo is equal to V than this indicate cycle is not present 
+        if(topo.size()==V) return false;
+        //else present
+        return true;
+ }
+
+
 //TC: O(N)+O(N+E) ~ O(N+E)
 //SC: O(N)
